095be052f1f275c4c2db0049ab4de58cd8f9079b
xdong002 <xxx419168445@gmail.com>
2017-09-21 21:30:15 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = []
+
+var cardOne = cards[0]
+cardsInPlay.push(cardOne);
+
+var cardTwo = cards[2]
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+
+	else {
+		alert("Sorry, try again.");
+	}
+}
+
+
+console.log("User flipped " + cardsInPlay);
